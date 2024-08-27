ff0c61c9f21603b7db9c53d6229d420647d3715e
Ceazar <razaecj23@yahoo.com>
2024-08-28 01:15:55 +0200

    Updated app.py to bind to 0.0.0.0 and use dynamic port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -56,5 +57,5 @@ def suggestions():
     return jsonify(suggestions)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
